49a4ba18594f7ef303ebd39d476062119fc97cd3
Phoebe Muweebya <phoebemuweebya@gmail.com>
2026-07-01 16:02:07 +0300

    feat(factory): add TopicFactory for generating topic model instances

---
+++ database/factories/TopicFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Group;
+use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Topic>
+ */
+class TopicFactory extends Factory
+{
+    protected $model = Topic::class;
+
+    public function definition(): array
+    {
+        return [
+            'group_id' => Group::factory(),
+            'created_by' => User::factory(),
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'status' => 'active',
+            'post_type' => 'discussion',
+        ];
+    }
+
+    public function question(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'post_type' => 'question',
+            ];
+        });
+    }
+
+    public function archived(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => 'archived',
+            ];
+        });
+    }
+}
