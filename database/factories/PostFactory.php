ea856c208651f3cb456a1fce49fe160136b51a99
Phoebe Muweebya <phoebemuweebya@gmail.com>
2026-07-01 16:01:59 +0300

    feat(factory): create PostFactory for generating post model instances

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\Topic;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    protected $model = Post::class;
+
+    public function definition(): array
+    {
+        return [
+            'topic_id' => Topic::factory(),
+            'user_id' => User::factory(),
+            'content' => $this->faker->paragraph,
+            'is_removed' => false,
+            'is_reported' => false,
+        ];
+    }
+
+    public function reported(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'is_reported' => true,
+            ];
+        });
+    }
+
+    public function removed(): static
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'is_removed' => true,
+            ];
+        });
+    }
+}
